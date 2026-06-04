6023366f2f83bc5918a7142922c6a6b0f5336387
EdouardoRabe <edouardorabendrazana@gmail.com>
2026-06-04 17:20:14 +0300

    clean code

--- src/pages/BO/BOReset.jsx
+++ src/pages/BO/BOReset.jsx
@@ -1,4 +1,4 @@
-import {useState, useEffect } from "react"
+import {useState} from "react"
 import { toDelete, reset } from "../../backend/services/reset/reset";
 
 export default function BOReset() {
