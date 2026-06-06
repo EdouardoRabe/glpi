56d62eb3dcc22ea6412d819951620e1543bcc487
EdouardoRabe <edouardorabendrazana@gmail.com>
2026-06-06 16:49:05 +0300

    add Asset

--- src/backend/model/Ticket.js
+++ src/backend/model/Ticket.js
@@ -1,8 +1,7 @@
 import api from "../utils/api";
 import apiV1 from "../utils/apiV1"
 import { clause, and, or, fetchAll } from "../utils/query";
-import Computer from "./Computer";
-import Monitor from "./Monitor";
+import Asset from "./Asset";
 
 class Ticket {
 
@@ -67,16 +66,13 @@ class Ticket {
         return items;
     }
 
-    async getItemsAssets(){
+    async getItemsAssets() {
         const items = await this.getItems();
-        const assets = await Promise.all(items.map(item => {
-            return item.itemtype === "Computer"
-                ? Computer.getById(item.items_id)
-                : Monitor.getById(item.items_id);
-        }));
+        const assets = await Promise.all(
+            items.map(item => Asset.getByIdSimple(item.itemtype, item.items_id))
+        );
         return assets;
     }
-
     async getCosts(){
         const cost = await api.get( `Assistance/Ticket/${this.id}/Cost`);
         return cost;
