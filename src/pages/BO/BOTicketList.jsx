af9874357ca3d7c9b674a97de2f545ce7f8f3061
EdouardoRabe <edouardorabendrazana@gmail.com>
2026-06-08 10:27:27 +0300

    Update BOTicketList.jsx

--- src/pages/BO/BOTicketList.jsx
+++ src/pages/BO/BOTicketList.jsx
@@ -220,6 +220,7 @@ export default function BOTicketList() {
                                 |  Time Cost: { selectedTicket.costs.reduce((acc, cost ) => {return acc + cost.cost_time ;}, 0)}
                                 |  Fixed Cost: { selectedTicket.costs.reduce((acc, cost ) => {return acc + cost.cost_fixed ;}, 0)}
                                 </strong></p>
+                                <strong> Total : { (selectedTicket.costs.reduce((acc, cost ) => {return acc + cost.duration ;}, 0) * selectedTicket.costs.reduce((acc, cost ) => {return acc + cost.cost_time ;}, 0) /3600) + selectedTicket.costs.reduce((acc, cost ) => {return acc + cost.cost_fixed ;}, 0)}</strong>
                             </div>
                         )}
                     </div>
