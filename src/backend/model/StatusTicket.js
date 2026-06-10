84449777a607809b680c4e5286fdb1167d7bca7f
EdouardoRabe <edouardorabendrazana@gmail.com>
2026-06-10 09:52:55 +0300

    change to lowercase

--- src/backend/model/StatusTicket.js
+++ src/backend/model/StatusTicket.js
@@ -20,22 +20,22 @@ class StatusTicket {
     }
 
     static getByEnglishName(status, englishName){
-        const filtered = status.filter((status) => status.english_name === englishName);
+        const filtered = status.filter((status) => status?.english_name.toLowerCase() === englishName.toLowerCase());
         return filtered.length > 0 ? filtered [0] : null;
     }
 
     static getByFrenchName(status, frenchName){
-        const filtered = status.filter((status) => status.french_name === frenchName);
+        const filtered = status.filter((status) => status?.french_name.toLowerCase() === frenchName.toLowerCase());
         return filtered.length > 0 ? filtered [0] : null;
     }
 
     static getByMalagasyName(status, MalagasyName){
-        const filtered = status.filter((status) => status.malagasy_name === MalagasyName);
+        const filtered = status.filter((status) => status?.malagasy_name.toLowerCase() === MalagasyName.toLowerCase());
         return filtered.length > 0 ? filtered [0] : null;
     }
 
     static getByIdStatus(status, idStatus){
-        const filtered = status.filter((status) => status.id_status === idStatus);
+        const filtered = status.filter((status) => status?.id_status === idStatus);
         return filtered.length > 0 ? filtered [0] : null;
     }
 }
